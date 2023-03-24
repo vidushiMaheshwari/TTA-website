2e982520ad6a9ec97e27904b8cfb699cea53d714
Ishwarya Sundhararajan <80136488+iSundhararajan@users.noreply.github.com>
2023-03-23 20:00:41 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './screens/Home/Home';
 import './App.css';
 import About from './screens/About/About';
 import Challenges from './screens/Challenges/Challenges';
+import Get_Involved from './screens/Get_Involved/Get_Involved';
 import NavBar from './screens/NavBar/NavBar';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Challenges />
+      <Get_Involved />
     </>
   );
 }
