7da8a5aebb43b43573fa42e2fa618e918439f9b8
vidushiMaheshwari <vidushi.maheshwari04@gmail.com>
2023-03-27 09:40:02 -0400

    tickets images added -- css required

--- src/screens/Get_Involved/Get_Involved.jsx
+++ src/screens/Get_Involved/Get_Involved.jsx
@@ -2,36 +2,35 @@ import React from "react"
 import classes from "./Get_Involved.module.css";
 import { Image } from "react-bootstrap";
 import logo from "../../public/tta-logo.png";
+import ticket from "../../public/get-involved-ticket.png";
 import NavBar from "../NavBar/NavBar";
 
 function Get_Involved() {
     return (
       <div className={classes.mainContainer}>
-        <div className={classes.brand}>
-          <div style={{ width: "7vh" }}>
-            <Image src={logo} style={{ width: "100%", height: "auto" }} />
+        <div className={classes.content}>
+          <div className={classes.ticketContainerLeft}>
+            <a href="https://organize.mlh.io/participants/events/9242-techtogether-atlanta">
+              <Image src={ticket} className={classes.ticket}></Image>
+            </a>
+            <br />
+            Hacker
           </div>
-          <div>
-            <div style={{ fontWeight: "600" }}>TechTogether</div>
-            <div>Get Involved</div>
-          </div>
-        </div>
 
-         <div className={classes.rect}>
-          <div className={classes.text}>
-            <p>
-              <a href="https://organize.mlh.io/participants/events/9242-techtogether-atlanta
-              ">Hacker</a>
-            </p>
-            <p>
-              <a href="https://organize.mlh.io/participants/events/9242-techtogether-atlanta
-              <a href=">Mentor</a>
-            </p>
-            <p>
-              <a href="https://organize.mlh.io/participants/events/9242-techtogether-atlanta
-            <a href=">Judge</a>
-            </p>
-            
+          <div className={classes.ticketContainerRight}>
+            <a href="https://organize.mlh.io/participants/events/9242-techtogether-atlanta">
+              <Image src={ticket} className={classes.ticket}></Image>
+            </a>
+            <br />
+            Judge
+          </div>
+
+          <div className={classes.ticketContainerLeft}>
+            <a href="https://organize.mlh.io/participants/events/9242-techtogether-atlanta">
+              <Image src={ticket} className={classes.ticket}></Image>
+            </a>
+            <br />
+            Mentor
           </div>
         </div>
       </div>
